4fe2978bc986bdfd67bac142294ad453ae02230b
gcancomert@hotmail.com <138134393+Galanovic@users.noreply.github.com>
2025-10-24 15:50:52 +0300

    fixed on project.

---
+++ src/main/java/com/gokhancomert/b2bapplication/service/FileUploadService.java
@@ -0,0 +1,54 @@
+package com.gokhancomert.b2bapplication.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Objects;
+import java.util.UUID;
+
+@Service
+public class FileUploadService {
+
+    private static final Logger logger = LoggerFactory.getLogger(FileUploadService.class);
+
+    // application.properties'tan dosya yükleme dizinini okur
+    @Value("${file.upload-dir}")
+    private String uploadDir;
+
+    /**
+     * Yüklenen dosyayı sunucuya kaydeder, dosyanın erişilebilir URL'ini döner.
+     * @param - file yüklenecek dosya(MultipartFile)
+     * @return Kaydedilen dosyanın URL'i
+     * @throws "IOException" Dosya kaydetme sırasında bir hata oluşursa..
+     */
+    private String uploadFile(MultipartFile file) throws IOException {
+        //Dosya adını temizle ve benzersiz hale getir
+        String originalFileName = Objects.requireNonNull(file.getOriginalFilename());
+        String fileName = UUID.randomUUID().toString() + "_" + originalFileName.replaceAll("\\s+", "_");
+        logger.info("Uploading file {}", originalFileName, fileName);
+
+        //Yükleme dizinini oluşturuyoruz(yoksa)
+        Path copyLocation = Paths.get(uploadDir + "/" + fileName);
+        Files.createDirectories(copyLocation.getParent());
+
+        //Dosyayı hedef konuma kopyala
+        Files.copy(file.getInputStream(), copyLocation, StandardCopyOption.REPLACE_EXISTING);
+        logger.info("File saved to: {}", copyLocation.toAbsolutePath());
+
+        //Dosyanın erişilebilir URL'ini oluştur ve döndür
+        //Bu URL, uygulamamızın çalıştığı adrese göre dinamik olarak oluşturur.
+        return ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/uploads/")// app.properties'te yer alan upload-dir ile eşleşmeli.
+                .path(fileName)
+                .toUriString();
+    }
+}
