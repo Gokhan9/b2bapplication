b79df5f2849585c9ab67ffc954ae1de76d996ebc
gcancomert@hotmail.com <138134393+Galanovic@users.noreply.github.com>
2025-11-04 16:37:43 +0300

    LoginRequest anotasyon eklendi.

--- src/main/java/com/gokhancomert/b2bapplication/dto/request/UserLoginRequest.java
+++ src/main/java/com/gokhancomert/b2bapplication/dto/request/UserLoginRequest.java
@@ -1,5 +1,7 @@
 package com.gokhancomert.b2bapplication.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserLoginRequest {
+
+    @NotBlank(message = "Kullanıcı Adı Boş Olamaz.")
     private String username;
+
+    @NotBlank(message = "Şifre Boş Olamaz.")
+    @Size(min = 8, message = "Şifre En Az 8 Karakter Uzunluğunda Olmalıdır.")
     private String password;
 }
