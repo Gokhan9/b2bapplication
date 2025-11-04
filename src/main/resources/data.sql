758d5ad856d9ae02a61df636095e59a0d099b413
gcancomert@hotmail.com <138134393+Galanovic@users.noreply.github.com>
2025-11-04 20:39:29 +0300

    ProductControllerIntegrationTest productları dönen ve id'ye göre product getiren veya id'ye göre product bulamadığında 404 fırlatan entegrasyon testleri eklendi.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO category (id, name) VALUES (1, 'Test Kategorisi');
+
+INSERT INTO product (id, name, description, price, stock, in_stock, category_id) VALUES (1, "Test Ürünü", "Bu bir test ürünü..", 150.0, 10, true, 1);
